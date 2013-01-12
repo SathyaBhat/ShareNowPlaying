895822ca6edc469e411342465dae195e51568379
SathyaBhat <sathya@sathyasays.com>
2013-01-12 23:59:27 +0530

    Initial POC

---
+++ ShareNowPlaying/Share.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ShareNowPlaying
+{
+    partial class Share
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCLT = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "What\'s playing? ♫";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCLT
+            // 
+            this.txtCLT.Location = new System.Drawing.Point(19, 61);
+            this.txtCLT.Name = "txtCLT";
+            this.txtCLT.Size = new System.Drawing.Size(235, 20);
+            this.txtCLT.TabIndex = 1;
+            // 
+            // Share
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtCLT);
+            this.Controls.Add(this.button1);
+            this.Name = "Share";
+            this.Text = "Share Now Playing ♫";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCLT;
+    }
+}
+
---
+++ ShareNowPlaying/Share.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using iTunesLib;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShareNowPlaying
+{
+    public partial class Share : Form
+    {
+        public Share()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            iTunesApp app = new iTunesApp();
+            IITTrack track = app.CurrentTrack;
+           txtCLT.Text = "\"" + track.Name + "\" by " +  track.Artist;
+
+        } 
+    }
+}
